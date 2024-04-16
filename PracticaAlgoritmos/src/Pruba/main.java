ee77d8db6544968ca62c78affdbb12c7f9085325
wugena <162334776+wugena@users.noreply.github.com>
2024-04-16 17:19:24 +0200

    metodo factorail creado

--- PracticaAlgoritmos/src/Pruba/main.java
+++ PracticaAlgoritmos/src/Pruba/main.java
@@ -3,7 +3,6 @@ package Pruba;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- PracticaAlgoritmos/src/Uilt/Algoritmos.java
+++ PracticaAlgoritmos/src/Uilt/Algoritmos.java
@@ -3,13 +3,38 @@ package Uilt;
 public abstract class Algoritmos {
 	
 	
-	 public int fibonacci () {
-	        
+	
+
+	/**
+	 * Recibe un número entero y devuelve el factorial de dicho número.
+
+	 * @param Factorial para saber el factoral
+	 * @return devuelve funcio factorial si introduce el numero te devulve el funcion
+	 */
+	 public int factorial (int Factorial) {
+	        for(int i =0; i<Factorial ; i++) {
+	        	Factorial *=i;
+	        }		 
+	        return Factorial;
+	    }
+	 /**
+	  * primos Recibe un número entero y devuelve true o false 
+	  * en función de si es primo o no.
+	  * @param primos para averiguar el entre 0 si queda resto o si si queda resto 
+	  * no es nun numero primo si es divisibre entre cero da error
+	  * @return Devuleve si es verdadero o falso el numero 
+	  */
+
+	 
+	 public boolean  esPrimos (int primos) {
+		 boolean Esprimos  ; 
+	        if(primos /2 == 0) {
+	        	Esprimos=true;
+	        }else
+	        	Esprimos=false;
+		 return Esprimos;
 		 
-		 
-		 
-	        return 0;
 	    }
 
-		
+
 }
